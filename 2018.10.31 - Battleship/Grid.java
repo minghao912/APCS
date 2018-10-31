40921273ee50615909d2e30097e24aa883900ca7
minghao912 <minghaochen912@gmail.com>
2018-10-31 16:21:46 -0700

    new battleship project

---
+++ 2018.10.31 - Battleship/Grid.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event;
+import javax.swing.*;
+
+public class Grid {
+    JPanel attempt1 = new JPanel();
+
+    GridLayout experiment = new GridLayout(10, 10);
+    attempt1.setLayout(experiment);
+}
