7124413c89144dcbb71d254916970d26f4fefbcc
minghao912 <minghaochen912@gmail.com>
2018-10-31 20:37:23 -0700

    got 10x10 button grid working, changing button to red

--- 2018.10.31 - Battleship/Grid.java
+++ 2018.10.31 - Battleship/Grid.java
@@ -1,22 +1,52 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.Arrays;
 
-public class Grid extends JFrame {
-    int rows = 10;
-    int columns = 10;
+public class Grid extends JFrame implements ActionListener {
+    private int rows = 10;
+    private int columns = 10;
+    private JButton[][] buttons; // Create 2D array of buttons
 
     public Grid(int inputRows, int inputColumns) {
         rows = inputRows;
         columns = inputColumns;
+        buttons = new JButton[rows][columns];
+    }
+    
+    void setFrameTitle(String title) {
+        this.setTitle(title);
     }
 
     void createGrid() {
         Container pane = getContentPane();
+        pane.setBackground(Color.lightGray);
         pane.setLayout(new GridLayout(rows, columns));
-        for (int i = 0; i < rows * columns; i++) {
-            JButton button = new JButton((i + 1) + "");
-            pane.add(button);
+
+        //Create 100 buttons, all at a preferred size of 40x40
+        int buttonCount = 0;    
+        String buttonID;
+        for (int y = 0; y < columns; y++) {
+            for (int x = 0; x < rows; x++) {
+                buttonID = ++buttonCount + "";
+
+                buttons[y][x] = new JButton();
+                buttons[y][x].setPreferredSize(new Dimension(40, 40));
+                buttons[y][x].setName(buttonID);
+                buttons[y][x].addActionListener(this);
+                buttons[y][x].setActionCommand(x + ", " + y);
+                pane.add(buttons[y][x]);
+            }
         }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        //Get button coordinate
+        String actionCoordinate = e.getActionCommand();
+        String[] xy = actionCoordinate.split(", ");
+        int x = Integer.parseInt(xy[0]);
+        int y = Integer.parseInt(xy[1]);
+
+        buttons[y][x].setBackground(Color.red);
     }
 }
--- 2018.10.31 - Battleship/test.java
+++ 2018.10.31 - Battleship/test.java
@@ -4,8 +4,10 @@ public class test {
     public static void main(String[] args) {
         Grid testGrid = new Grid(10, 10);
         testGrid.createGrid();
+        testGrid.setFrameTitle("Battlehsip");
         testGrid.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         testGrid.pack();
+        testGrid.setLocationRelativeTo(null);
         testGrid.setVisible(true);
     }
 }
