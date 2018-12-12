528331dafb605c50d99edc6f3733cb28929d0e10
minghao912 <minghaochen912@gmail.com>
2018-12-11 21:09:09 -0800

    did holiday graphics

--- 2018.12.10 - Sample Graphics/Holiday.java
+++ 2018.12.10 - Sample Graphics/Holiday.java
@@ -1,26 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Holiday implements ActionListener{
+public class Holiday {
     private static JFrame win;
     private static Container contentPane;
     private static Graphics g;
     public static void main(String[] args) {
         Holiday holiday = new Holiday();
         holiday.makeFrame();
-        
-        ActionListener kylehasuglygraphics = new ActionListener() {
-            public void actionPerformed(ActionEvent kenneedsglasses) {
+
+        new PlaySound().play("./LetItSnow2.wav", -10.0f);
+
+        Timer yeet = new Timer();
+        TimerTask activateWiper = new TimerTask() {
+            @Override
+            public void run() {
+                holiday.wiper();
+            }
+        };
+        yeet.schedule(activateWiper, 5000, 10000);
+
+        Timer memes = new Timer();
+        TimerTask kylehasuglygraphics = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Yo!");
                 holiday.drawBackground();
             }
         };
+        memes.schedule(kylehasuglygraphics, 0, 100);
 
-        sleep(100);
-        holiday.drawThings();
-
-        new Timer(500, kylehasuglygraphics);
+        Timer endingSequence = new Timer();
+        TimerTask endCard = new TimerTask() {
+            @Override
+            public void run() {
+                holiday.end();
+            }
+        };
+        endingSequence.schedule(endCard, 115000);
     }
 
     public static void sleep(int t) {
@@ -44,6 +63,8 @@ public class Holiday implements ActionListener{
         g = contentPane.getGraphics();
     }
 
+    /*
+    Tree
     public void drawThings() {
         g.setColor(new Color(110, 65, 5));
         g.fillRect(350, 300, 100, 150);
@@ -78,26 +99,69 @@ public class Holiday implements ActionListener{
         g.setColor(new Color(0, 40, 10));
         g.fillRect(0, 450, 800, 300);
     }
+    */
 
     public void drawBackground() {
+        System.out.println("Drawing bakgoround");
         contentPane.setBackground(new Color(25, 40, 60));
         
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 100; i++) {
             int x = randNum(0, 800);
             int y = randNum(0, 600);
             int length = randNum(2, 5);
+            int thickness = randNum(1, 3);
 
             g.setColor(new Color(200, 250, 255));
-            g.drawLine(x, y, x, y + length);
+            g.fillOval(x, y, thickness, length);
         }
+
+        /*         
+        sleep(100);
+        System.out.println("Drawing objets.");
+        drawThings(); 
+        */
+    }
+
+    public void wiper() {
+        for (int deg = 0; deg <= 90; deg++) {
+            g.setColor(new Color(25, 40, 60));
+
+            g.fillArc(-800, 25, 1600, 1600, 0, deg);
+            System.out.println("squeak");
+            sleep(50);
+        }
+    }
+
+    public void end() {
+        JFrame end = new JFrame("Happy Holidays!");
+        end.setPreferredSize(new Dimension(800, 600));
+        end.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        end.setLocationRelativeTo(null);
+
+        String[] stuff = { "Season's Greetings!", "Happy Holidays!", "-Ming" };
+
+        JPanel inner = new JPanel();
+        inner.setLayout(new GridBagLayout());
+        inner.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        Box box = new Box(BoxLayout.Y_AXIS);
+        box.add(box.createVerticalGlue());
+        for (String thing : stuff) {
+            JLabel label = new JLabel("<html><h1><div style='text-align: center;'>" + thing + "</div></h1></html>", SwingConstants.CENTER);
+            label.setFont(new Font("Sans_Serif", Font.BOLD, 36));
+            box.add(label);
+        }
+        box.add(box.createVerticalGlue());
+        
+        inner.add(box);
+        end.add(inner);
+        end.pack();
+        end.setVisible(true);
+
+        win.dispose();
     }
 
     public static int randNum(int min, int max) {
         return (int) (Math.random() * (max - 1) + 1);
-    }
-
-
-    @Override
-    public void actionPerformed(ActionEvent ae) {
     }
 }
---
+++ 2018.12.10 - Sample Graphics/PlaySound.java
@@ -0,0 +1,41 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.net.URL;
+import java.io.*;
+import javax.sound.sampled.*;
+
+/**
+ * Plays sounds.
+ */
+public class PlaySound {
+    /**
+     * Play a sound.
+     * 
+     * @param sound - a {@code String} filename of the sound file within
+     *              the directory {@code ./Files/Sounds/}
+     */
+    public void play(String sound, float volume) {
+        try {
+            //Retreive sound
+            URL url = this.getClass().getClassLoader().getResource(sound);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+            Clip clip = AudioSystem.getClip();
+
+            //Change volume
+            clip.open(audioIn);
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(volume);
+
+            //Play sound
+            clip.start();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+// 20/11/2018 10:03
