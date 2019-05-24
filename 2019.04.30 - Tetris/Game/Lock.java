9b4561e3a5cbcaac09d71e5ff0b0f146d8a16847
minghao912 <minghaochen912@gmail.com>
2019-05-23 19:39:48 -0700

    a,rzsd mik.luthykjawh354ekrhdfvk

--- 2019.04.30 - Tetris/Game/Lock.java
+++ 2019.04.30 - Tetris/Game/Lock.java
@@ -1,5 +1,11 @@
 package Game;
 
+/**
+ * This class serves no purpose but
+ * to act as a lock for synchronization.
+ */
 public class Lock {
     public Lock() {}
 }
+
+// https://docs.oracle.com/javase/tutorial/essential/concurrency/locksync.html
